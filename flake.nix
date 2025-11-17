8d8c0c083d5db097fddb8c1cbefc1e04ef78913d
Matt <151405417+mattking340@users.noreply.github.com>
2025-11-16 20:25:23 -0500

    update

--- flake.nix
+++ flake.nix
@@ -6,40 +6,60 @@
   };
 
   outputs = { self, nixpkgs, ... }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
     {
-      nixosConfigurations.myhost = nixpkgs.lib.nixosSystem {
+      nixosConfigurations.dorian = pkgs.lib.nixosSystem {
         system = "x86_64-linux";
 
         modules = [
           ./hardware-configuration.nix
 
           {
-            networking.hostName = "dorian";
+            networking = {
+              hostName = "dorian";
+              networkmanager.enable = true;
+            };
 
-            services.xserver.enable = true;
-            services.xserver.displayManager.gdm.enable = true;
-            services.xserver.desktopManager.gnome.enable = true;
+            services = {
+              xserver = {
+                enable = true;
+                displayManager.lightdm.enable = true;
+                desktopManager.xfce.enable = true;
+              };
+            };
 
             users.users.matt = {
               isNormalUser = true;
               extraGroups = [ "wheel" "networkmanager" ];
             };
-
             security.sudo.wheelNeedsPassword = false;
 
-            environment.systemPackages = with nixpkgs.pkgs; [
+            environment.systemPackages = with pkgs; [
               firefox
-              vscode
+              codium
               git
               neovim
+              ghc
+              cabal-install
+              haskell-language-server
+              fourmolu
+              python3
             ];
+
+            programs.vscode = {
+              enable = true;
+              package = pkgs.codium;
+              extensions = with pkgs.vscode-extensions; [
+                haskell.haskell
+              ];
+            };
 
             nixpkgs.config.allowUnfree = true;
 
             sound.enable = true;
             hardware.pulseaudio.enable = true;
-
-            networking.networkmanager.enable = true;
 
             time.timeZone = "America/Toronto";
             i18n.defaultLocale = "en_CA.UTF-8";
